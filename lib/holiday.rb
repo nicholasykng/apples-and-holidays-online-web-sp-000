7a8273f8c4ed872e021b63202a2501c220c971a1
Nicholas Ng <nicholasykng@gmail.com>
2019-10-21 02:08:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,6 +51,11 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash[season][holid]
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, item|
+      holiday if item.include?("BBQ")
+    end
+  end
+        
+      end
 
-end
